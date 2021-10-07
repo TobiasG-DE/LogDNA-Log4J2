e177357a5b5b7beb9871a666c58deea51a68e2e1
Tobias G <tobiasdevde@gmail.com>
2021-10-07 18:30:10 +0200

    Update LogDNAScheduledAsyncAppender.java

--- src/main/java/eu/mizerak/alemiz/LogDNAScheduledAsyncAppender.java
+++ src/main/java/eu/mizerak/alemiz/LogDNAScheduledAsyncAppender.java
@@ -42,6 +42,7 @@ public class LogDNAScheduledAsyncAppender extends LogDNASimpleAppender {
 
     @Override
     public void append(LogEvent logEvent) {
+        if(logEvent.getLevel().intLevel() <= this.minimalLogLevel.intLevel()) return;
         Layout<? extends Serializable> layout = this.getLayout();
         String message = new String(layout.toByteArray(logEvent));
 
